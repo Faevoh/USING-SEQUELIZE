5a5807f471953e39c0e96ad703428bc3c54ba374
unknown <favourigbani29@gmail.com>
2022-12-18 22:41:12 +0100

    Added create and update to use of sequelize while operating CRUD

--- CONTROLLER/control.js
+++ CONTROLLER/control.js
@@ -1,5 +1,6 @@
 import { coffeeModel } from "../MODEL/model.js";
 
+//to display all coffee in list
 export const allCoffee = async (req,res)=>{
     const coffee = await coffeeModel.findAll();
     try{
@@ -9,11 +10,103 @@ export const allCoffee = async (req,res)=>{
             });
         }else{
             res.status(200).json({
-                message: "Displaying allCoffees",
-                data: rows
+                message: "Displaying allCoffees " + coffee.length,
+                data: coffee
             });
         }
     }catch(error){
         console.log(err.message)
+    }
+};
+
+//to display single coffee
+export const coffeeSingle = async(req, res)=>{
+    try{
+        const id = req.params.id;
+        const singleCoffee = await coffeeModel.findAll({
+            where:{
+                id: id
+            }
+        });
+        if(singleCoffee.length === 0){
+            res.status(404).json({
+                message: `id ${id} doesnt exist`
+            });
+        }else{
+            res.status(200).json({
+                message: `Id ${id} : `,
+                data: singleCoffee
+            });
+        }
+    }catch(error){
+        res.status(404).json({
+            message: err.message
+        });
+    }
+};
+
+//to create or add to the coffee list
+export const newCoffee = async(req, res)=>{
+    try{
+        const newcoffees = await coffeeModel.create(req.body);
+        res.status(200).json({
+            message: "New Coffee Added to list",
+            data: newcoffees
+        });
+    }catch(error){
+        res.status(404).json({
+            message: error.message
+        });
+    }
+};
+
+//to update an existing coffee
+export const updateCoffee = async(req,res)=>{
+    try{
+        const id = req.params.id;
+        const updatedCoffee = await coffeeModel.update(req.body,{
+            where: {
+                id: id
+            }
+        });
+        if(updatedCoffee[0] === 0){
+            res.status(404).json({
+                message: `Coffee with id${id} not updated`
+            });
+        }else{
+            res.status(200).json({
+                message: `Coffee has been updated`,
+                data: updatedCoffee
+            });
+        }
+    }catch(error){
+        res.status(404).json({
+            message: error.message
+        });
+    }
+};
+
+//to delete a coffee
+export const deleteCoffee = async(req,res)=>{
+    try{
+        const id = req.params.id;
+        const deletedCoffee = await coffeeModel.destroy({
+            where: {
+                id: id
+            }
+        });
+        if(deletedCoffee === 0){
+            res.status(404).json({
+                message: `Coffee with id ${id} not found`
+            });
+        }else{
+            res.status(200).json({
+                message: `Coffee with id ${id} has been deleted.`
+            });
+        }
+    }catch(error){
+        res.status(404).json({
+            message: error.message
+        });
     }
 };
--- ROUTES/route.js
+++ ROUTES/route.js
@@ -1,6 +1,14 @@
 import express from "express";
-import { allCoffee } from "../CONTROLLER/control.js";
+import { allCoffee ,coffeeSingle, newCoffee, updateCoffee, deleteCoffee} from "../CONTROLLER/control.js";
 import { Sequelize } from "sequelize";
 
  export const coffeeRoute = express.Router();
 coffeeRoute.get("/coffee", allCoffee);
+//display single coffee
+coffeeRoute.get("/coffee/:id", coffeeSingle);
+//creating new coffee
+coffeeRoute.post("/coffee", newCoffee);
+//update coffee
+coffeeRoute.patch("/coffee/:id", updateCoffee);
+//delete coffee
+coffeeRoute.delete("/coffee/:id", deleteCoffee);
